abff9193f09ac35ff1cd5a47f354bb572e7a2bee
joaodmonteiro <joaodmonteiro10@gmail.com>
2022-07-02 15:27:11 +0100

    home branch

--- pages/index.js
+++ pages/index.js
@@ -3,5 +3,18 @@ import Image from "next/image";
 import styles from "../styles/Home.module.css";
 
 export default function Home() {
-  return <div className={styles.container}></div>;
+  return <div>hello</div>;
+}
+
+export async function getServerSideProps() {
+  const resp = await fetch(
+    `https://maps.googleapis.com/maps/api/directions/json?origin=london&destination=leeds&mode=transit&key=${process.env.REACT_APP_GOOGLE_MAPS_API}`
+  );
+
+  const data = await resp.json();
+  console.log(data);
+
+  return {
+    props: {},
+  };
 }
